90d68d0e012660d542185276c393566310c3de27
tcsrushabh <tcsrushabh@gmail.com>
2021-08-09 12:26:02 +0000

    My first commit

---
+++ src/com/tcs/entities/Gymnasium.java
@@ -0,0 +1,22 @@
+package com.tcs.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gymnasium {
+	static List<User> users =  new ArrayList<User>();
+	public static void main(String[] args) {
+		createUsers();
+	}
+	private static void createUsers() {
+		for(int i=0;i<10;i++) {
+			User u1 = new User();
+			u1.setName("Ram " +i);
+			u1.setAge((byte)(10 * i));
+			users.add(u1);
+		}
+		System.out.println(users);
+		users.get(1).workout();
+	}
+
+} 
