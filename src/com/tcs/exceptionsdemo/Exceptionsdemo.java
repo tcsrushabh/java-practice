e4bb4c35963de4b9f72f2ae79367ef6cc1de2267
tcsrushabh <tcsrushabh@gmail.com>
2021-08-11 07:46:39 +0000

    using throws handled in main method

--- src/com/tcs/exceptionsdemo/Exceptionsdemo.java
+++ src/com/tcs/exceptionsdemo/Exceptionsdemo.java
@@ -1,10 +1,15 @@
 package com.tcs.exceptionsdemo;
 
 public class Exceptionsdemo {
-	public static void main(String[] args) throws MyCheckedException {
-		Demo2();
+	public static void main(String[] args){
+		try {
+			Demo2();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	private static void Demo2() throws MyCheckedException {
+	private static void Demo2() throws MyCheckedException{
 		throw new MyCheckedException();
 	}
 	private static void Demo1() {
