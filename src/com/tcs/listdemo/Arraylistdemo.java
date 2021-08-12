114d89ee2f0ff0af11e982a5d49d7099cd33af04
tcsrushabh <tcsrushabh@gmail.com>
2021-08-12 06:14:20 +0000

    List Features Demo

--- src/com/tcs/listdemo/Arraylistdemo.java
+++ src/com/tcs/listdemo/Arraylistdemo.java
@@ -3,14 +3,30 @@ package com.tcs.listdemo;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class Arraylistdemo {
 
 	public static void main(String[] args) {
 		List<Integer> myList = createList();
+		//SortList(myList);
+		filterList(myList);
+	}
+
+	private static void filterList(List<Integer> myList) {
+		myList.forEach((number) ->{
+			System.out.println(number % 2 == 0);
+		});
+		List<Integer> filter = myList.stream()
+						.filter((number) -> number % 2 == 0)
+						.collect(Collectors.toList());
+		System.out.println(filter);
+	}
+
+	private static void SortList(List<Integer> myList) {
 		System.out.println(myList);
 		myList.sort(new Comparator<Integer>() {
-
 			@Override
 			public int compare(Integer o1, Integer o2) {
 				return o2 - o1;
